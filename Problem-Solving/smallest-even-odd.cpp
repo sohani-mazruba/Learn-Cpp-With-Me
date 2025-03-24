39b797aed745eb5195e94cb5e7617d0578fbde90
sohani-mazruba <174067409+sohani-mazruba@users.noreply.github.com>
2025-03-25 03:55:29 +0600

    added smallest-num

---
+++ Problem-Solving/smallest-even-odd.cpp
@@ -0,0 +1,36 @@
+/*
+write a C++ program to find the smallest even or odd number among three integers
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3;
+
+    cout << "Enter three integers: ";
+    cin >> num1 >> num2 >> num3;
+
+    int smallest = num1;
+
+    if (num2 < smallest)
+        {
+        smallest = num2;
+        }
+
+   if (num3 < smallest)
+        {
+        smallest = num3;
+        }
+
+    cout << "The smallest number is: " << smallest << endl;
+
+    if (smallest % 2 == 0) {
+        cout << "The smallest number is even." << endl;
+    } else {
+        cout << "The smallest number is odd." << endl;
+    }
+
+    return 0;
+}
---
+++ Problem-Solving/smallest-num.cpp
@@ -0,0 +1,31 @@
+/*
+Write a C++ program that finds the smallest number among three integers
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3;
+
+    cout << "Enter three integers: ";
+    cin >> num1 >> num2 >> num3;
+
+    int smallest = num1;
+
+    if (num2 < smallest)
+        {
+        smallest = num2;
+        }
+
+   else if (num3 < smallest)
+        {
+        smallest = num3;
+        }
+
+    cout << "The smallest number is: " << smallest << endl;
+
+    return 0;
+
+}
