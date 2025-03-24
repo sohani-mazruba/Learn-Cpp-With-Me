2b9caa34693a367a3bd6f03c6d95265faad604d7
sohani-mazruba <174067409+sohani-mazruba@users.noreply.github.com>
2025-03-25 03:36:05 +0600

    added Problem-Solving

---
+++ Problem-Solving/Largest-Number.cpp
@@ -0,0 +1,31 @@
+/*
+ Write a C++ program to find the largest number among three integers entered by the user.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3;
+
+    cout << "Enter three integers: ";
+    cin >> num1 >> num2 >> num3;
+
+    int largest = num1;
+
+    if (num2 > largest)
+        {
+        largest = num2;
+        }
+
+   if (num3 > largest)
+        {
+        largest = num3;
+        }
+
+    cout << "The largest number is: " << largest << endl;
+
+    return 0;
+}
+
