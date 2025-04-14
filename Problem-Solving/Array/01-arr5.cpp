f6c45f42b70aa477fa3de2f670461acf5240b5bc
sohani-mazruba <174067409+sohani-mazruba@users.noreply.github.com>
2025-04-14 15:46:29 +0600

    added Problem-Solving

---
+++ Problem-Solving/Array/01-arr5.cpp
@@ -0,0 +1,24 @@
+/*
+ Create a C++ program that takes input from the user to store 5 integers in a one-dimensional
+ array and then displays the elements of the array.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int arr[5];
+
+    cout << "Enter 5 integers: ";
+    for (int i=0; i<5; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The elements in the array are: " <<" " << endl;
+    for (int i=0; i<5; i++) {
+        cout << arr[i]<<" ";
+    }
+
+    return 0;
+}
---
+++ Problem-Solving/Array/02-sum-of-all-elements.cpp
@@ -0,0 +1,19 @@
+/*
+Write a C++ program to find the sum of all elements in a one-dimensional array of integers.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int arr[5], sum;
+
+    for (int i=0; i<5; i++) {
+        cout << "Enter element: " << i+1 << ": ";
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << "Sum of all element is: " << sum << endl;
+
+    return 0;
+}
